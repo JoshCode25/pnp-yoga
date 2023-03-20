b394ed22eb534fb05131dafd056185f0f8c9bae0
JoshG <joshcode25@gmail.com>
2023-03-20 12:40:37 -0500

    updated header paths and placeholder logo

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,11 +1,29 @@
 <header>
-	<a href="/about">About</a>
-	<a href="/contact">Contact</a>
-	<a href="writings">Writings</a>
+	<a class="logoAnchor" href="/">peace & presence yoga</a>
+	<div class="navWrapper">
+		<a href="/">Home</a>
+		<a href="/about">About</a>
+		<a href="/contact">Contact</a>
+		<a href="/writings">Writings</a>
+	</div>
 </header>
 
 <style>
 	header {
+		width: 100%;
+	}
+	.logoAnchor {
+		display: inline-block;
+		margin: 1rem;
+		font-size: 2rem;
+		font-family: Arial, Helvetica, sans-serif;
+	}
+	.logoAnchor:visited {
+		color: var(--primary-dark);
+	}
+	div.navWrapper {
 		display: flex;
+		justify-content: space-around;
+		background: var(--primary-dark);
 	}
 </style>
