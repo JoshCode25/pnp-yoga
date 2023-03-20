e9a10dae4f11d19454465d5bfe115a5699233ce3
JoshG <joshcode25@gmail.com>
2023-03-20 06:44:26 -0500

    populated header and landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import lizard from '$lib/images/lizard.jpg';
+</script>
+
+<div class="flexWrapper">
+	<img src={lizard} alt="lizard yoga pose" />
+	<div>
+		<h1>Experiencing God's Peace</h1>
+		<p>
+			by encountering His presence through movement, breath, & meditation on His precious Living
+			Word.
+		</p>
+		<p>
+			Would you like to schedule a faith baed yoga Instructor? Wondering how <a href="/writings"
+				>faith &#43; movement</a
+			> intersect?
+		</p>
+		<p>Curious about <a href="/about">my journey</a>?</p>
+	</div>
+</div>
+
+<style>
+	.flexWrapper {
+		display: flex;
+		gap: 2rem;
+		margin: 2rem;
+	}
+
+	img {
+		width: 40rem;
+		border-radius: 20rem;
+	}
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,11 @@
+<header>
+	<a href="/about">About</a>
+	<a href="/contact">Contact</a>
+	<a href="writings">Writings</a>
+</header>
+
+<style>
+	header {
+		display: flex;
+	}
+</style>
