bf2e03e5d2d648f652cab07d8a787cc69d728eec
JoshG <joshcode25@gmail.com>
2023-03-20 13:00:23 -0500

    initial population of writings page

---
+++ src/lib/components/BlogPreview.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import defaultThumbnail from '$lib/images/default.jpg';
+
+	export let name = 'Blog Title';
+	export let summary = 'Blog Summary';
+	export let thumbnailImage = defaultThumbnail;
+</script>
+
+<div class="flexWrapper grow">
+	<img src={thumbnailImage} alt="thumbnail description" />
+	<div class="textWrapper">
+		<h2>{name}</h2>
+		<p>{summary}</p>
+	</div>
+</div>
+
+<style>
+	.flexWrapper {
+		display: flex;
+		gap: 2rem;
+		margin: 2rem;
+	}
+	.flexWrapper:hover {
+		cursor: pointer;
+	}
+
+	img {
+		width: 10rem;
+		border-radius: 1rem;
+	}
+
+	.textWrapper {
+		margin: 1rem 2rem;
+	}
+	.textWrapper * {
+		text-align: left;
+	}
+
+	p {
+		margin: 1rem;
+	}
+</style>
---
+++ src/routes/writings/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import BlogPreview from '$lib/components/BlogPreview.svelte';
+	import reverseWarrior from '$lib/images/reverse-warrior.jpg';
+</script>
+
+<BlogPreview />
+<BlogPreview thumbnailImage={reverseWarrior} />
+<BlogPreview />
+<BlogPreview />
