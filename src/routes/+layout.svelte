13d0ce820f7be580e6556f31e91d6dddfbb3fad8
JoshG <joshcode25@gmail.com>
2023-03-21 06:06:46 -0500

    set up sample olive branch background

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,40 @@
 	import './defaultStyles.css';
 	import './defaultAnimations.css';
 	import Header from './Header.svelte';
+	import oliveBranch from '$lib/images/sample-olive-branch.png';
 </script>
 
 <body>
 	<Header />
 	<slot />
+	<div class="background">
+		<img src={oliveBranch} alt="olive branch" class="branch1" />
+		<img src={oliveBranch} alt="olive branch" class="branch2" />
+	</div>
 </body>
+
+<style>
+	.background {
+		position: absolute;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		z-index: -10;
+	}
+
+	img {
+		width: 50%;
+		position: absolute;
+		opacity: 0.05;
+	}
+	.branch1 {
+		left: 0;
+		bottom: 0;
+	}
+	.branch2 {
+		right: 0;
+		top: 0;
+		transform: rotate3d(0, 0, 1, 180deg);
+	}
+</style>
